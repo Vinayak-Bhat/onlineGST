96ef64dea1e4fc6c6784fa328c9db24fbb7992f3
root <vinayak.g@oulook.com>
2017-11-30 20:19:49 +0530

    added joinBBB mobile parameter

--- lms/urls.py
+++ lms/urls.py
@@ -31,8 +31,8 @@ urlpatterns = (
 
     url(r'', include('student.urls')),
     # TODO: Move lms specific student views out of common code
-    url(r'^courses/{}/joinbbb/'.format(settings.COURSE_ID_PATTERN), 'student.views.joinBBB', name="joinBBB"),
-    url(r'^courses/{}/joinbbb/(?P<mobile>\w+)/$'.format(settings.COURSE_ID_PATTERN), 'student.views.joinBBB', name="joinBBB"),
+    url(r'^courses/{}/joinbbb/$'.format(settings.COURSE_ID_PATTERN), 'student.views.joinBBB', name="joinBBB"),
+    url(r'^courses/{}/joinbbb/(?P<mobile>\D+)/$'.format(settings.COURSE_ID_PATTERN), 'student.views.joinBBB', name="joinBBB"),
 
     url(r'^dashboard$', 'student.views.dashboard', name="dashboard"),
     url(r'^mycourses$', 'student.views.mycourses', name="mycourses"),
